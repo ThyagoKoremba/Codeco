a672337c131227bf9f47530f8c59445e01445202
Thyago Koremba <tkoremba@gmail.com>
2025-03-18 17:39:21 -0300

    comm

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
+
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
@@ -15,8 +16,10 @@ createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
+        <DashboardLayout>
 
         root.render(<App {...props} />);
+        </DashboardLayout>
     },
     progress: {
         color: '#4B5563',
